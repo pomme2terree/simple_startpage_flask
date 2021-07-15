acd6a269f8c0a76c3f537cb6cdb86158988c2092
Takeus <68923554+pomme2terree@users.noreply.github.com>
2021-07-15 22:24:31 +0200

    Updates

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
-import os, time
+import os
 from flask import *
 
 app = Flask(__name__)
 app.secret_key = os.urandom(16)
 
-# Value Config
-username = 'Takeus'
-Logo = True
 
-# Backgrounds
+# Info Config
+title = 'StartPage'
+logo = True
+logo_text = 'My StartPage' # Str (IF logo=False)
+logo_img = 'logo_flaticon_freepik.png' # in static/background/ (IF logo=True)
+
+# Backgrounds Config
 background = False
 background_color = '#95a5a6' # Hex (IF Background=False)
-background_img = 'alphacoders_background_by_mimosa.jpg' # in static/background/ (IF Background=True)
+background_img = 'alphacoders_background_by_mimosa.jpg' # in static/background/ (IF background=True)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html',title=username,background=background,background_color=background_color,background_img=url_for('static',filename='background/'+str(background_img)))
+    return render_template('index.html',title=title, logo=logo,logo_text=logo_text,logo_img=url_for('static',filename='/'+str(logo_img)),background=background,background_color=background_color,background_img=url_for('static',filename='background/'+str(background_img)))
 
 @app.route('/<string:link>',methods=['post', 'get'])
 def redirect_link(link):
